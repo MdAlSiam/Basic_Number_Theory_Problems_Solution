028cd234f5d8dd7752f6e8bdd9568659d04b9d42
Md. Al Siam <highambitioussiam@gmail.com>
2020-02-03 22:32:09 +0600

    Create SPOJ ALICESIE.cpp

---
+++ SPOJ ALICESIE.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <utility>
+#include <list>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define dd double
+#define scl(x) scanf("%lld", &x)
+#define scd(x) scanf("%lf", &x)
+#define pi acos(-1.0)
+#define pb push_back
+#define Sort(x) sort(x.begin(), x.end())
+#define For(i, x, y) for(ll i = x; i < y; i++)
+#define pii pair < int, int >
+#define ppi pair < pii, int >
+#define ff first
+#define ss second
+
+int main(){
+	ll test, n;
+	scl(test);
+	while(test--){
+		scl(n);
+		if(n%2) printf("%lld\n", (n/2)+1);
+		else printf("%lld\n", (n/2));
+	}
+	return 0;
+}
